5b8cb322d5e19ca9f2b112b7fb1fdeca4c9cbc29
Chuck Harmston <chuck@chuckharmston.com>
2012-03-23 21:23:31 -0400

    Adds RSS feed to URLconf

--- tumblelog/urls.py
+++ tumblelog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 
+from tumblelog.feeds import PostFeed
 from tumblelog.views import PostDetailView, PostListView
 
 urlpatterns = patterns('tumblelog.views',
     url(r'^$', PostListView.as_view(), name="list"),
+    url(r'^feed/$', PostFeed(), name="feed"),
     url(r'^(?P<slug>.+)/$', PostDetailView.as_view(), name="detail"),
 )
